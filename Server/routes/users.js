5e224661989b787fec42a4421c091a4682acb9f3
Anthony <betovem19@hotmail.com>
2023-10-23 23:05:11 -0300

    add opt

--- Server/routes/users.js
+++ Server/routes/users.js
@@ -15,6 +15,14 @@ let transporter = nodemailer.createTransport({
     }
 });
 
+router.options("/", (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.sendStatus(200);
+});
+
+
 router.post("/", async (req, res) => {
     try {
         const { error } = validate(req.body);
